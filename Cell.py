ce12674be2cc2e8688fb975c5837c942b6cbefe2
dvilela <dvilelaf@gmail.com>
2020-11-07 18:02:52 +0100

    Bugfix: mating needs spawn position

--- Cell.py
+++ Cell.py
@@ -29,7 +29,7 @@ class Cell:
             self.lifePoints = sum([p.lifePoints for p in parents]) / len(parents)
 
             for gen in self.genes:
-                self.genes[gen] = sum([p.lifegenes[gen] for p in parents]) / len(parents)
+                self.genes[gen] = sum([p.genes[gen] for p in parents]) / len(parents)
                 weight += self.genes[gen]
 
         # Gen normalization
@@ -61,29 +61,42 @@ class Cell:
         for action in actionSet:
             actionSet[action] /= actionWeight
 
-        # Combine genetic weight and environmental weight
+        # Combine genetic weight and environmental weight and select action
         combinedWeights=[self.genes[action] * actionSet[action] for action in actionSet]
         action = random.choices(list(actionSet.keys()), weights=combinedWeights, k=1)[0]
 
+        # Select target
         targetPositions = []
+        spawnPositions = []
 
         if action == 'wait':
             targetPositions.append(4) # 4 is the center of the environment, self
                                       # 0 1 2
                                       # 3 4 5
                                       # 6 7 8
-
-        elif action in ['move', 'mate']:
+        elif action == 'move':
             for i in range(len(environment)):
                 if environment[i] == 'empty':
                     targetPositions.append(i)
+
+        elif action == 'mate':
+            for i in range(len(environment)):
+                if environment[i] == 'friend':
+                    targetPositions.append(i)
+
+            for i in range(len(environment)):
+                if environment[i] == 'empty':
+                    spawnPositions.append(i)
 
         elif action in ['attack', 'changeTeam']:
             for i in range(len(environment)):
                 if environment[i] == 'foe':
                     targetPositions.append(i)
 
-        newPosition = random.choice(targetPositions)
-        positionDelta = (int(newPosition / 3) - 1, newPosition % 3 - 1)
+        targetPosition = random.choice(targetPositions)
+        targetPositionDelta = (int(targetPosition / 3) - 1, targetPosition % 3 - 1)
 
-        return action, positionDelta
+        spawnPosition = random.choice(spawnPositions) if len(spawnPositions) > 0 else None
+        spawnPositionDelta = (int(spawnPosition / 3) - 1, spawnPosition % 3 - 1) if spawnPosition != None else None
+
+        return action, targetPositionDelta, spawnPositionDelta
--- World.py
+++ World.py
@@ -75,26 +75,29 @@ class World:
                                     foesNeighbours += 1
                                     environment.append('foe')
 
-                action, positionDelta = cell.selectAction(environment)
-                position = ((cell.row + positionDelta[0]) % self.height, (cell.col + positionDelta[1]) % self.width)
-                target = self.get(position[0], position[1])
+                action, targetPositionDelta, spawnPositionDelta = cell.selectAction(environment)
+
+                targetPosition = ((cell.row + targetPositionDelta[0]) % self.height, (cell.col + targetPositionDelta[1]) % self.width)
+                target = self.get(targetPosition[0], targetPosition[1])
+
+                spawnPosition = ((cell.row + spawnPositionDelta[0]) % self.height, (cell.col + spawnPositionDelta[1]) % self.width) if spawnPositionDelta else None
+
                 performedActions[action] += 1
 
                 if action == 'wait':
                     pass
 
                 elif action == 'move':
-                    cell.row = position[0]
-                    cell.col = position[1]
+                    cell.row = targetPosition[0]
+                    cell.col = targetPosition[1]
                     cell.normalizePosition(self.width, self.height)
 
                 elif action == 'mate':
                     cell.lifePoints = int((1 - cellMatingFactor) * cell.lifePoints)
-                    self.cells.append(Cell(cell.team, position[0], position[1], parents=(cell, target)))
-                    self.set(position[0], position[1], self.cells[-1]) # Keep map synced
+                    self.cells.append(Cell(cell.team, spawnPosition[0], spawnPosition[1], parents=(cell, target)))
+                    self.set(targetPosition[0], targetPosition[1], self.cells[-1]) # Keep map synced
 
                 elif action == 'attack':
-                    target = self.get(position[0], position[1])
                     lifeDelta = min((target.lifePoints, cell.lifePoints))
                     cell.lifePoints -= lifeDelta
                     target.lifePoints -= lifeDelta
