f38798d518340baf7bd36e7a02ef6c99f3e64309
dvilela <dvilelaf@gmail.com>
2020-11-07 21:02:04 +0100

    Do not inherit life points

--- Cell.py
+++ Cell.py
@@ -13,21 +13,15 @@ class Cell:
         self.col = col
         self.age = 0
         self.genes = {'wait': 0.0, 'move': 0.0, 'mate': 0.0, 'attack': 0.0, 'changeTeam': 0.0}
+        self.lifePoints = random.uniform(cellMinStartingLifeFactor * self.maxLifePoints, self.maxLifePoints)
 
         weight = 0.0
 
         if not parents:
-
-            self.lifePoints = random.uniform(cellMinStartingLifeFactor * self.maxLifePoints, self.maxLifePoints)
-
             for gen in self.genes:
                 self.genes[gen] = random.random() * geneticMask[self.team][gen]
                 weight += self.genes[gen]
-
         else:
-
-            self.lifePoints = sum([p.lifePoints for p in parents]) / len(parents)
-
             for gen in self.genes:
                 self.genes[gen] = sum([p.genes[gen] for p in parents]) / len(parents)
                 weight += self.genes[gen]
