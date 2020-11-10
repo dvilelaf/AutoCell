7e707fde1afeff83479067ad2ef67549913b6160
dvilela <dvilelaf@gmail.com>
2020-11-10 17:59:24 +0100

    Optional window

--- AutoCell.py
+++ AutoCell.py
@@ -13,10 +13,13 @@ cellWidth = np.floor(windowWidth / worldWidth)
 cellHeight = np.floor(windowHeight / worldHeight)
 
 # Initialization
-pygame.init()
-screen = pygame.display.set_mode((windowWidth, windowHeight))
 world = World(worldWidth, worldHeight, initialPopulation=startingPopulation, nTeams=nTeams)
 pause = False
+
+if showWindow:
+    pygame.init()
+    screen = pygame.display.set_mode((windowWidth, windowHeight))
+
 plot = Plot(world) if showPlot else None
 video = SimVideoWriter() if writeVideo else None
 
@@ -27,28 +30,30 @@ while True:
     if not pause:
         world.step()
 
-        screen.fill(backgroundColor)
+        if showWindow:
+            screen.fill(backgroundColor)
 
-        # Grid
-        if gridOn:
-            for i in range(worldWidth):
-                pygame.draw.line(screen, gridColor, (i * cellWidth, 0), (i * cellWidth, windowHeight), width=1)
-            for j in range(worldHeight):
-                pygame.draw.line(screen, gridColor, (0, j * cellHeight), (windowWidth, j * cellHeight), width=1)
+            # Grid
+            if gridOn:
+                for i in range(worldWidth):
+                    pygame.draw.line(screen, gridColor, (i * cellWidth, 0), (i * cellWidth, windowHeight), width=1)
+                for j in range(worldHeight):
+                    pygame.draw.line(screen, gridColor, (0, j * cellHeight), (windowWidth, j * cellHeight), width=1)
 
-        # Cells
-        for cell in world.cells:
-            surface = pygame.Surface((cellWidth, cellHeight))
-            surface.fill(colors[cell.team])
+            # Cells
+            for cell in world.cells:
+                surface = pygame.Surface((cellWidth, cellHeight))
+                surface.fill(colors[cell.team])
 
-            ageFactor = (cell.maxAge - cell.age) / cell.maxAge         # 1 -> young,   0 -> old
-            lifePointsFactor = cell.lifePoints / cell.maxLifePoints    # 1 -> healthy, 0 -> damaged
-            surface.set_alpha(int(255.0 * (ageFactor + lifePointsFactor) / 2.0))
+                ageFactor = (cell.maxAge - cell.age) / cell.maxAge         # 1 -> young,   0 -> old
+                lifePointsFactor = cell.lifePoints / cell.maxLifePoints    # 1 -> healthy, 0 -> damaged
+                surface.set_alpha(int(255.0 * (ageFactor + lifePointsFactor) / 2.0))
 
-            screen.blit(surface, (cell.col * cellWidth, cell.row * cellHeight))
+                screen.blit(surface, (cell.col * cellWidth, cell.row * cellHeight))
+
+            pygame.display.flip()
 
         # Write video
-        pygame.display.flip()
         if writeVideo:
             video.writeFrame(world)
 
@@ -62,13 +67,14 @@ while True:
             sys.exit()
 
     # Keyboard event check
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            video.end()
-            pygame.quit()
-            sys.exit()
-        elif event.type == pygame.KEYDOWN:
-            pause = not pause
+    if showWindow:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                video.end()
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                pause = not pause
 
     # Sleep
     time.sleep(frameWait)
